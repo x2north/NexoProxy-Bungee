a4665ce1037cb7a053bb2f1ad2c183cdb71cbbda
Boy0000 <sivertpaulsen2@gmail.com>
2026-03-22 14:57:56 +0100

    chore: remove url

--- src/main/kotlin/com/nexomc/nexoproxy/pack/ResourcePackInfo.kt
+++ src/main/kotlin/com/nexomc/nexoproxy/pack/ResourcePackInfo.kt
@@ -8,18 +8,15 @@ data class ResourcePackInfo(
     val obfuscatedHash: String,
     val obfuscatedUuid: UUID = UUID.nameUUIDFromBytes(obfuscatedHash.hexToByteArray()),
     val uuid: PackUUID,
-    val url: String
 ) {
     constructor(json: JsonObject) : this(
         uuid = UUID.fromString(json.get("uuid").asString),
-        url = json.get("url").asString,
         obfuscatedHash = json.get("obfuscated").asString,
         unobfuscatedHash = json.get("unobfuscated").asString
     )
 
     fun toJson(): JsonObject = JsonObject().apply {
         addProperty("uuid", uuid.toString())
-        addProperty("url", url)
         addProperty("obfuscated", obfuscatedHash)
         addProperty("unobfuscated", unobfuscatedHash)
     }
